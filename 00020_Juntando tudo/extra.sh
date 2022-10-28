ed95c96b17215c8f36da5ec45a73fa2a51815888
franco@mumuki.org <franco@mumuki.org>
2022-10-28 19:21:26 +0000

    Mumuki Export on 2022-10-28 19:21:26 +0000

--- 00020_Juntando tudo/extra.sh
+++ 00020_Juntando tudo/extra.sh
@@ -3,29 +3,29 @@ cd curriculos
 cat <<HTML > curriculo-v1.html
 <html>
 <head>
-  <title>Mi Currículum</title>
+  <title>Meu Currículo</title>
   <meta charset="utf-8">
 </head>
 <body>
   <header>
-    <h1>Mi Currículum</h1>
+    <h1>Mi CV</h1>
   </header>
   <main>
     <section>
       <h3>Habilidades</h3>
       <ul>
-        <li>Programación con objetos</li>
+        <li>Programação com objetos</li>
         <li>Ruby</li>
         <li>HTML</li>
-        <li>Programación funcional</li>
-        <li>Haskell</li>
+        <li>Programação imperativa</li>
+        <li>Python</li>
       </ul>
     </section>
   </main>
 </body>
 </html>
 HTML
-sed 's:</ul>:<li>Gobstones</li></ul>:' curriculum-v1.html > curriculum-v2.html
+sed 's:</ul>:<li>Shell</li></ul>:' curriculum-v1.html > curriculum-v2.html
 sed 's:</ul>:<li>SQL</li></ul>:' curriculum-v2.html > curriculum-v3.html
-sed 's:</ul>:<li>Bases de datos</li></ul>:' curriculum-v3.html > curriculum-v4.html
+sed 's:</ul>:<li>Bases de dados</li></ul>:' curriculum-v3.html > curriculum-v4.html
 cd
