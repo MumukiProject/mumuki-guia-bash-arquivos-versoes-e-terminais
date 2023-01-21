2210baed696b6ec64e617f7c1496a5b1e9c8a23d
franco@mumuki.org <franco@mumuki.org>
2023-01-21 18:43:06 +0000

    Mumuki Export on 2023-01-21 18:43:06 +0000

--- 00018_The cat is under the terminal/extra.sh
+++ 00018_The cat is under the terminal/extra.sh
@@ -2,14 +2,14 @@ mkdir curriculos
 touch zaraza.txt
 cat > curriculos/curriculo-v1.html <<EOF
 <html>
-  <head>
+  <head>  
     <title>Meu Primeiro CV</title>
   </head>
 </html>
 EOF
 cat > curriculos/curriculo-v2.html  <<EOF
 <html>
-  <head>
+  <head>  
     <title>Meu Primeiro CV</title>
   </head>
 </html>
